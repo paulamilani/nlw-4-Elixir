4844e3a027eb6042b5a88e7662342f91988b1bfa
paulamilani <paula.millanii@gmail.com>
2021-02-27 18:05:58 -0300

    unit test

--- test/rocketpay/numbers_test.exs
+++ test/rocketpay/numbers_test.exs
@@ -8,10 +8,19 @@ defmodule Rocketpay.NumbersTest do
     test "when there is a file with the given name, returns the sum of numbers" do
       response = Numbers.sum_from_file("Numbers")
 
-      expected_response = "banana"
+      expected_response = {:ok, %{result: 37}}
 
       assert response == expected_response
     end
+
+    describe "sum_from_file/1" do
+      test "when there is no file with the given name, returnsan error" do
+        response = Numbers.sum_from_file("banana")
+
+        expected_response = {:error, %{message: "Invalid file!"}}
+
+        assert response == expected_response
+      end
   end
 
 end
